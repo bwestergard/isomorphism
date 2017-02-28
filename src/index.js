48a1ae3cded35d9d625b2a30328cac14c9def3c5
Bjorn Westergard <bjornw@gmail.com>
2017-02-28 17:37:56 -0500

    src/index
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,211 @@
+/* @flow */
+
+import { map, all, any, filter, reduce, concat } from 'ramda'
+
+/* Types */
+
+type RowVector = number[]
+
+type Matrix = RowVector[]
+
+export type DiGraph = Matrix
+export type Mapping = Matrix
+export type Isomorphism = number[]
+
+/* Utility */
+
+export const flatmap = <T,R>(fn: (x:T) => Array<R>, xs: Array<T>): Array<R> =>
+reduce(concat, [], map(fn, xs))
+
+/* Adjacency list graph representation */
+
+// In the directed graph `graph`, does `vertex` have an edge to `neighbor`?
+const isAdjacent = (
+  graph: DiGraph,
+  vertex: number,
+  neighbor: number
+): boolean =>
+{
+  for (let i = 0; i <= graph[vertex].length; i++) {
+    if (graph[vertex][i] > neighbor) return false // NOTE: adjacency list must be sorted ascending
+    if (graph[vertex][i] === neighbor) return true
+  }
+  return false
+}
+
+// What is the degree of `vertex` in `graph`?
+const deg = (
+  graph: DiGraph,
+  vertex: number
+) => graph[vertex].length
+
+/* Ullman */
+
+export const extractAtMostOneIsomorphism = (
+  pattern: DiGraph,
+  target: DiGraph,
+  mapping: Mapping
+): Isomorphism[] => {
+  const mappedTargetVertex = (patternVertex: number): number | null =>
+  mapping[patternVertex].length === 1
+  ? mapping[patternVertex][0]
+  : null
+
+  let iso: Isomorphism = []
+
+  for (let patternVertex = 0; patternVertex < pattern.length; patternVertex++) {
+    let targetVertexForPatternVertex = mappedTargetVertex(patternVertex)
+    if (targetVertexForPatternVertex === null) return []
+    let patternVector = pattern[patternVertex]
+    for (let j = 0; j < patternVector.length; j++) {
+      let patternNeighbor = patternVector[j]
+      let maybeTarget = mappedTargetVertex(patternNeighbor)
+      if (
+        maybeTarget === null ||
+        !isAdjacent(
+          target,
+          targetVertexForPatternVertex,
+          maybeTarget
+        )
+      ) {
+        return []
+      }
+    }
+    iso.push(targetVertexForPatternVertex)
+  }
+  return [iso]
+}
+
+// Given a Mapping, return a mapping in which patternVertex in mapped to targetVertex
+const setMappingInPossibleMappings = (
+  possibleMappings: Mapping,
+  patternVertex: number,
+  targetVertex: number
+) => {
+  let mapping = []
+  for (let i = 0; i < possibleMappings.length; i++) {
+    let possibleMappingsForPatternVertex = possibleMappings[i]
+    mapping.push(
+      i === patternVertex
+      ? [targetVertex]
+      : possibleMappingsForPatternVertex
+    )
+  }
+  return mapping
+}
+
+const refine = (
+  mapping: Mapping,
+  predicate: (x: number, possibleTarget: number) => boolean
+): Mapping | null => {
+  let refinedMapping: Mapping = []
+  for (let patternVertex = 0; patternVertex < mapping.length; patternVertex++) {
+    refinedMapping.push([])
+    let mappingVector = mapping[patternVertex]
+
+    if (mappingVector.length === 0) return null // No possible mapping!
+
+    for (let i = 0; i < mappingVector.length; i++) {
+      let possibleTarget = mappingVector[i]
+      if (predicate(patternVertex, possibleTarget)) {
+        refinedMapping[patternVertex].push(possibleTarget)
+      }
+    }
+  }
+
+  return refinedMapping
+}
+
+const degreeRefine = (
+  pattern: DiGraph,
+  target: DiGraph,
+  mapping: Mapping
+): Mapping | null =>
+refine(
+  mapping,
+  (patternVertex: number, targetVertex: number) =>
+  deg(pattern, patternVertex) <= deg(target, targetVertex)
+)
+
+const ullmanRefine = (
+  pattern: DiGraph,
+  target: DiGraph,
+  mapping: Mapping
+): Mapping | null =>
+refine(
+  mapping,
+  (patternVertex: number, targetVertex: number): boolean => all(
+    (patternVertexNeighbor) => any(
+      (targetVertexNeighbor) => isAdjacent(mapping, patternVertexNeighbor, targetVertexNeighbor),
+      target[targetVertex]
+    ),
+    pattern[patternVertex]
+  )
+)
+
+const search = (
+  pattern: DiGraph,
+  target: DiGraph,
+  possibleMappings: Mapping,
+  scanVertex: number
+): Isomorphism[] =>
+{
+  const maybeRefined = ullmanRefine(
+    pattern,
+    target,
+    possibleMappings
+  )
+  return !maybeRefined
+  ? []
+  : scanVertex < possibleMappings.length
+    ? flatmap(
+      (speculativeTargetMappingForScanVertex) => search(
+        pattern,
+        target,
+        setMappingInPossibleMappings(
+          maybeRefined,
+          scanVertex,
+          speculativeTargetMappingForScanVertex
+        ),
+        scanVertex + 1
+      ),
+      possibleMappings[scanVertex]
+    )
+    : extractAtMostOneIsomorphism(pattern, target, possibleMappings)
+}
+
+const allMappings = (
+  patternOrder: number,
+  targetOrder: number
+): Mapping => {
+  let mapping: Mapping = []
+  for (let i = 0; i < patternOrder; i++) {
+    mapping.push([])
+    for (let j = 0; j < targetOrder; j++) {
+      mapping[i].push(j)
+    }
+  }
+  return mapping
+}
+
+export const allIsomorphisms = (
+  pattern: DiGraph,
+  target: DiGraph,
+  initialpossibleMappings: ?Mapping
+): Isomorphism[] => {
+  const maybeRefined = degreeRefine(
+    pattern,
+    target,
+    initialpossibleMappings || allMappings(pattern.length, target.length)
+  )
+  return !maybeRefined
+    ? []
+    : pattern.length > target.length
+      ? []
+      : search(
+        pattern,
+        target,
+        maybeRefined,
+        0
+      )
+}
