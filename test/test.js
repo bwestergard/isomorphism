3f3e5d81c3ec9f1884f1d889e743ac83af15f373
Bjorn Westergard <bjornw@gmail.com>
2017-02-28 17:57:44 -0500

    testing and building working, git and npm ignores configured
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

---
+++ test/test.js
@@ -0,0 +1,198 @@
+/* @flow */
+/* global describe, it */
+
+import { clone } from 'ramda'
+import assert from 'assert'
+import { allIsomorphisms } from '../lib/index'
+
+declare class describe {
+  static (description: string, spec: () => void): void;
+}
+
+declare class it {
+  static (description: string, spec: () => void): void;
+}
+
+// TODO test for every error type
+
+describe('Isomorphism', function () {
+
+  it('a -> b has only one isomorphism to a -> b', function () {
+    assert.deepEqual(
+      allIsomorphisms(
+        [
+          [1],
+          []
+        ],
+        [
+          [1],
+          []
+        ],
+        null
+      ),
+      [
+        [0,1]
+      ]
+    )
+  })
+
+  it('Three-cycle isomorphic to every cycle', function () {
+    assert.deepEqual(
+      allIsomorphisms(
+        [
+          [1],
+          [2],
+          [0]
+        ],
+        [
+          [1],
+          [2],
+          [0]
+        ],
+        null
+      ),
+      [
+        [0,1,2],
+        [1,2,0],
+        [2,0,1]
+      ]
+    )
+  })
+
+  it('Isomorphisms for three-chain pattern on ten-chain', function () {
+    assert.deepEqual(
+      allIsomorphisms(
+        [
+          [1],
+          [2],
+          []
+        ],
+        [
+          [1],
+          [2],
+          [3],
+          [4],
+          [5],
+          [6],
+          [7],
+          [8],
+          [9],
+          []
+        ],
+        null
+      ),
+      [
+        [0,1,2],
+        [1,2,3],
+        [2,3,4],
+        [3,4,5],
+        [4,5,6],
+        [5,6,7],
+        [6,7,8],
+        [7,8,9]
+      ]
+    )
+  })
+
+  it('Isomorphisms for three-chain pattern on ten-cycle', function () {
+    assert.deepEqual(
+      allIsomorphisms(
+        [
+          [1],
+          [2],
+          []
+        ],
+        [
+          [1],
+          [2],
+          [3],
+          [4],
+          [5],
+          [6],
+          [7],
+          [8],
+          [9],
+          [0]
+        ],
+        null
+      ),
+      [
+        [0,1,2],
+        [1,2,3],
+        [2,3,4],
+        [3,4,5],
+        [4,5,6],
+        [5,6,7],
+        [6,7,8],
+        [7,8,9],
+        [8,9,0],
+        [9,0,1]
+      ]
+    )
+  })
+
+	// TODO firecracker graph and Y?
+
+  it('Pattern larger than graph produces no isomorphisms', function () {
+    assert.deepEqual(
+      allIsomorphisms(
+        [
+          [1],
+          [2],
+          [3],
+          [4],
+          [5],
+          [6],
+          [7],
+          [8],
+          [9],
+          [0]
+        ],
+        [
+          [1],
+          [2],
+          []
+        ],
+        null
+      ),
+      []
+    )
+  })
+
+	// http://mathworld.wolfram.com/HajosGraph.html
+  it('Three-cycle has twelve isomorpisms to a certain directed Hajos graph', function () {
+    assert.deepEqual(
+      allIsomorphisms(
+        [
+          [1],
+          [2],
+          [0]
+        ],
+        [
+          [1],
+          [2,3],
+          [0,4],
+          [4],
+          [1,5],
+          [2]
+        ],
+        null
+      ),
+      [
+        [0,1,2],
+        [1,2,0],
+        [1,2,4],
+        [1,3,4],
+        [2,0,1],
+        [2,4,1],
+        [2,4,5],
+        [3,4,1],
+        [4,1,2],
+        [4,1,3],
+        [4,5,2],
+        [5,2,4]
+      ]
+    )
+  })
+
+})
